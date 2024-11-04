7c89f0f4196db7b234092631e5b4cb2c6dd4a721
Tusharbecoding <tusharchopra02122002@gmail.com>
2024-11-04 13:54:08 +0530

    main.go updated

---
+++ cmd/server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/tusharbecoding/argocd-clone/pkg/gitops"
+	"github.com/tusharbecoding/argocd-clone/pkg/k8s"
+)
+
+func main() {
+    config, err := gitops.LoadConfig("configs/config.yaml")
+    if err != nil {
+        log.Fatal("Failed to load config:", err)
+    }
+
+    client, err := k8s.NewK8sClient(config.Kubernetes.Kubeconfig)
+    if err != nil {
+        log.Fatal("Failed to create Kubernetes client:", err)
+    }
+
+    if err := gitops.Sync(config, client); err != nil {
+        log.Fatal("Sync failed:", err)
+    }
+}
